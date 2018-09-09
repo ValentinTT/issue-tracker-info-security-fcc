98bf2a88783b8b82f9018d92cea9f81009556915
Glitch (issue-tracker-info-security-fcc) <none>
2018-09-09 00:40:38 +0000

    🍹🍽 Updated with Glitch

--- server.js
+++ server.js
@@ -23,12 +23,6 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/:project/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
-  });
-
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
